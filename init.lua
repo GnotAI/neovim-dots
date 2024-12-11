896c7b2b80658812d631b5eb5005fb9fa6050f11
GnotAI <alleluiabriggs@gmail.com>
2024-12-11 13:50:39 +0100

    rem: options.lua

--- init.lua
+++ init.lua
@@ -1,13 +1,20 @@
+local o = vim.opt
+
 vim.cmd("set expandtab")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set softtabstop=2")
 vim.cmd("set tabstop=2")
 
-vim.opt.undofile = true  -- Turn on persistent undo
-vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undo"  -- Set the undo directory
+-- Options
+o.cmdheight = 0
+o.number = true
+o.swapfile = false
+o.relativenumber = true
+o.guicursor = "n-v-c-sm-i-ci-ve-cr:block"
+o.undofile = true  -- Turn on persistent undo
+o.undodir = os.getenv("HOME") .. "/.config/nvim/undo"  -- Set the undo directory
 
+-- External file configs
 require("drew.lazy")
 require("drew.lspconfig")
 require("drew.mappings")
-require("drew.options")
 require("plugins.init")
--- lua/drew/options.lua
+++
@@ -1,6 +0,0 @@
-local o = vim.opt
-o.number = true
-o.relativenumber = true
-o.guicursor = "n-v-c-sm-i-ci-ve-cr:block"
-o.swapfile = false
-o.cmdheight = 0
